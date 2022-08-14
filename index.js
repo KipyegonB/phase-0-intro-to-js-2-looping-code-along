cb9aef3ec257deed20fc34d1d523dfd1cb6a9990
KipyegonB <koechbernard08@gmail.com>
2022-08-14 15:47:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+function writeCards(names, event){
+    const newArray=[];
+for(let i=0;i<names.length;i++){
+    const newMessage=`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    newArray.push(newMessage);
+    }
+    return newArray;
+        
+}
+console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+
+//while loop
+function countDown(){
+    let countDown=10;
+    while (countDown >= 0){
+        console.log(countDown--);
+
+    }
+}
